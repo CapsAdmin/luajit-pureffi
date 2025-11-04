6da6b16c449bb5d8cff0158eb67fba4833319b5e
Elias Hogstvedt <eliashogstvedt@gmail.com>
2025-11-04 07:07:03 +0100

    refactoring

--- examples/vulkan_macos.lua
+++ examples/vulkan_macos.lua
@@ -5,35 +5,221 @@ local threads = require("threads")
 local wnd = cocoa.window()
 local vk = vulkan.vk
 local lib = vulkan.lib
-local instance = vulkan.CreateInstance({"VK_KHR_surface", "VK_EXT_metal_surface"})
-local surface = instance:CreateMetalSurface(assert(wnd:GetMetalLayer()))
-local physicalDevice = instance:GetPhysicalDevices()[1]
-local graphicsQueueFamily = physicalDevice:FindGraphicsQueueFamily(surface)
-local device = physicalDevice:CreateDevice({"VK_KHR_swapchain"}, graphicsQueueFamily)
-local surfaceFormat = physicalDevice:GetSurfaceFormats(surface)[0]
-local surfaceCapabilities = physicalDevice:GetSurfaceCapabilities(surface)
-local swapchain = device:CreateSwapchain(surface, surfaceFormat, surfaceCapabilities)
-local swapchainImages = swapchain:GetImages()
-local commandPool = device:CreateCommandPool(graphicsQueueFamily)
-local commandBuffer = commandPool:CreateCommandBuffer()
-local imageAvailableSemaphore = device:CreateSemaphore()
-local renderFinishedSemaphore = device:CreateSemaphore()
-local inFlightFence = device:CreateFence()
-local deviceQueue = device:GetQueue(graphicsQueueFamily)
+
+do -- renderer
+	local Renderer = {}
+	Renderer.__index = Renderer
+	-- Default configuration
+	local default_config = {
+		-- Swapchain settings
+		present_mode = "VK_PRESENT_MODE_FIFO_KHR", -- FIFO (vsync), IMMEDIATE (no vsync), MAILBOX (triple buffer)
+		image_count = nil, -- nil = minImageCount + 1 (usually triple buffer)
+		surface_format_index = 0, -- Which format from available formats to use
+		composite_alpha = "VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR", -- OPAQUE, PRE_MULTIPLIED, POST_MULTIPLIED, INHERIT
+		clipped = true, -- Clip pixels obscured by other windows
+		image_usage = nil, -- nil = COLOR_ATTACHMENT | TRANSFER_DST, or provide custom flags
+		-- Image acquisition
+		acquire_timeout = ffi.cast("uint64_t", -1), -- Infinite timeout by default
+		-- Presentation
+		pre_transform = nil, -- nil = use currentTransform
+	}
+
+	function Renderer.New(config)
+		local self = setmetatable({}, Renderer)
+		config = config or {}
+
+		-- Merge config with defaults
+		for k, v in pairs(default_config) do
+			self.config = self.config or {}
+			self.config[k] = config[k] ~= nil and config[k] or v
+		end
+
+		self:Initialize()
+		return self
+	end
+
+	function Renderer:Initialize()
+		-- Vulkan initialization
+		self.instance = vulkan.CreateInstance({"VK_KHR_surface", "VK_EXT_metal_surface"})
+		self.surface = self.instance:CreateMetalSurface(assert(wnd:GetMetalLayer()))
+		self.physical_device = self.instance:GetPhysicalDevices()[1]
+		self.graphics_queue_family = self.physical_device:FindGraphicsQueueFamily(self.surface)
+		self.device = self.physical_device:CreateDevice({"VK_KHR_swapchain"}, self.graphics_queue_family)
+		self.command_pool = self.device:CreateCommandPool(self.graphics_queue_family)
+		-- Query surface capabilities
+		self.surface_formats = self.physical_device:GetSurfaceFormats(self.surface)
+		self.surface_capabilities = self.physical_device:GetSurfaceCapabilities(self.surface)
+		self.present_modes = self.physical_device:GetPresentModes(self.surface)
+
+		-- Validate format index
+		if self.config.surface_format_index >= #self.surface_formats then
+			error(
+				"Invalid surface_format_index: " .. self.config.surface_format_index .. " (max: " .. (
+						#self.surface_formats - 1
+					) .. ")"
+			)
+		end
+
+		-- Build swapchain config
+		local swapchain_config = {
+			present_mode = self.config.present_mode,
+			image_count = self.config.image_count or (self.surface_capabilities[0].minImageCount + 1),
+			composite_alpha = self.config.composite_alpha,
+			clipped = self.config.clipped,
+			image_usage = self.config.image_usage,
+			pre_transform = self.config.pre_transform,
+		}
+		-- Create swapchain
+		self.swapchain = self.device:CreateSwapchain(
+			self.surface,
+			self.surface_formats[self.config.surface_format_index + 1],
+			self.surface_capabilities,
+			swapchain_config
+		)
+		self.swapchain_images = self.swapchain:GetImages()
+		-- Create command buffer
+		self.command_buffer = self.command_pool:CreateCommandBuffer()
+		-- Create synchronization objects
+		self.image_available_semaphore = self.device:CreateSemaphore()
+		self.render_finished_semaphore = self.device:CreateSemaphore()
+		self.in_flight_fence = self.device:CreateFence()
+		-- Get queue
+		self.queue = self.device:GetQueue(self.graphics_queue_family)
+		return self
+	end
+
+	function Renderer:PrintCapabilities()
+		print("Available surface formats (unique):")
+		local seen_formats = {}
+
+		for i, format in ipairs(self.surface_formats) do
+			local format_str = vk.EnumToString(format.format)
+			local colorspace_str = vk.EnumToString(format.colorSpace)
+			local key = format_str .. "|" .. colorspace_str
+
+			if not seen_formats[key] then
+				seen_formats[key] = true
+				print("  [" .. (i - 1) .. "] " .. format_str .. " / " .. colorspace_str)
+			end
+		end
+
+		print("\nAvailable present modes (unique):")
+		local seen_present_modes = {}
+
+		for _, mode in ipairs(self.present_modes) do
+			local modeStr = vk.EnumToString(mode)
+
+			if not seen_present_modes[modeStr] then
+				seen_present_modes[modeStr] = true
+				print("  " .. modeStr)
+			end
+		end
+
+		print("\nSurface capabilities:")
+		print("  Min image count: " .. self.surface_capabilities[0].minImageCount)
+		print("  Max image count: " .. self.surface_capabilities[0].maxImageCount)
+		print(
+			"  Current extent: " .. self.surface_capabilities[0].currentExtent.width .. "x" .. self.surface_capabilities[0].currentExtent.height
+		)
+		print(
+			"  Current transform: " .. vk.EnumToString(self.surface_capabilities[0].currentTransform)
+		)
+		-- Decode composite alpha bitmask
+		print("  Supported composite alpha modes:")
+		local composite_alpha_flags = self.surface_capabilities[0].supportedCompositeAlpha
+		local alpha_modes = {
+			{bit = 0x1, name = "VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR"},
+			{bit = 0x2, name = "VK_COMPOSITE_ALPHA_PRE_MULTIPLIED_BIT_KHR"},
+			{bit = 0x4, name = "VK_COMPOSITE_ALPHA_POST_MULTIPLIED_BIT_KHR"},
+			{bit = 0x8, name = "VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR"},
+		}
+
+		for _, mode in ipairs(alpha_modes) do
+			if bit.band(composite_alpha_flags, mode.bit) ~= 0 then
+				print("    " .. mode.name)
+			end
+		end
+	end
+
+	function Renderer:BeginFrame()
+		-- Wait for previous frame
+		self.in_flight_fence:Wait()
+		-- Acquire next image
+		self.image_index = self.swapchain:GetNextImage(self.image_available_semaphore)
+		-- Reset and begin command buffer
+		self.command_buffer:Reset()
+		self.command_buffer:Begin()
+		-- Transition image to transfer dst
+		self.barrier = self.command_buffer:CreateImageMemoryBarrier(self.image_index, self.swapchain_images)
+		self.command_buffer:StartPipelineBarrier(self.barrier)
+		return self.command_buffer, self.image_index, self.swapchain_images
+	end
+
+	function Renderer:EndFrame()
+		-- Transition image to present
+		self.command_buffer:EndPipelineBarrier(self.barrier)
+		self.command_buffer:End()
+		-- Submit command buffer
+		self.queue:Submit(
+			self.command_buffer,
+			self.image_available_semaphore,
+			self.render_finished_semaphore,
+			self.in_flight_fence
+		)
+		-- Present
+		self.swapchain:Present(self.render_finished_semaphore, self.queue, self.image_index)
+	end
+
+	function Renderer:cleanup()
+		lib.vkDeviceWaitIdle(self.device.ptr[0])
+	end
+
+	-- Export to global scope
+	_G.Renderer = Renderer
+end
+
+local renderer = Renderer.New(
+	{
+		present_mode = "VK_PRESENT_MODE_FIFO_KHR",
+		image_count = nil, -- Use default (minImageCount + 1)
+		surface_format_index = 0,
+		composite_alpha = "VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR",
+	}
+)
+renderer:PrintCapabilities()
 wnd:Initialize()
 wnd:OpenWindow()
 local frame = 0
 
+local function hsv_to_rgb(h, s, v)
+	local c = v * s
+	local x = c * (1 - math.abs((h * 6) % 2 - 1))
+	local m = v - c
+	local r, g, b
+	local h6 = h * 6
+
+	if h6 < 1 then
+		r, g, b = c, x, 0
+	elseif h6 < 2 then
+		r, g, b = x, c, 0
+	elseif h6 < 3 then
+		r, g, b = 0, c, x
+	elseif h6 < 4 then
+		r, g, b = 0, x, c
+	elseif h6 < 5 then
+		r, g, b = x, 0, c
+	else
+		r, g, b = c, 0, x
+	end
+
+	return r + m, g + m, b + m
+end
+
 while not wnd:ShouldQuit() do
 	local events = wnd:ReadEvents()
-	inFlightFence:Wait()
-	local imageIndex = swapchain:GetNextImage(imageAvailableSemaphore)
-	commandBuffer:Reset()
-	commandBuffer:Begin()
-	local barrier = commandBuffer:CreateImageMemoryBarrier(imageIndex, swapchainImages)
-	commandBuffer:StartPipelineBarrier(barrier)
+	local commandBuffer, imageIndex, swapchainImages = renderer:BeginFrame()
 
-	do
+	do -- rendering
 		local range = vk.Box(
 			vk.VkImageSubresourceRange,
 			{
@@ -49,25 +235,17 @@ while not wnd:ShouldQuit() do
 			swapchainImages[imageIndex[0]],
 			"VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL",
 			vk.Box(vk.VkClearColorValue, {
-				float32 = {(frame % 360) / 360.0, 0.5, 0.8, 1.0},
+				float32 = {hsv_to_rgb(frame % 1, 1, 1)},
 			}),
 			1,
 			range
 		)
 	end
 
-	commandBuffer:EndPipelineBarrier(barrier)
-	commandBuffer:End()
-	deviceQueue:Submit(
-		commandBuffer,
-		imageAvailableSemaphore,
-		renderFinishedSemaphore,
-		inFlightFence
-	)
-	swapchain:Present(renderFinishedSemaphore, deviceQueue, imageIndex)
-	frame = frame + 1
-	threads.sleep(16)
+	renderer:EndFrame()
+	frame = frame + 0.01
+	threads.sleep(1)
 end
 
 -- Cleanup
-lib.vkDeviceWaitIdle(device.ptr[0])
+renderer:cleanup()
--- vulkan.lua
+++ vulkan.lua
@@ -12,8 +12,8 @@ local function vk_assert(result, msg)
 end
 
 do -- instance
-	local meta = {}
-	meta.__index = meta
+	local Instance = {}
+	Instance.__index = Instance
 
 	function vulkan.CreateInstance(extensions)
 		local appInfo = vk.Box(
@@ -43,20 +43,19 @@ do -- instance
 		)
 		local ptr = vk.Box(vk.VkInstance)()
 		vk_assert(lib.vkCreateInstance(createInfo, nil, ptr), "failed to create vulkan instance")
-		local instance = setmetatable({ptr = ptr}, meta)
+		local instance = setmetatable({ptr = ptr}, Instance)
 		return instance
 	end
 
-	function meta:__gc()
+	function Instance:__gc()
 		lib.vkDestroyInstance(self.ptr[0], nil)
 	end
 
 	do -- metal surface
-		local meta_instance = meta
-		local meta = {}
-		meta.__index = meta
+		local Surface = {}
+		Surface.__index = Surface
 
-		function meta_instance:CreateMetalSurface(metal_layer)
+		function Instance:CreateMetalSurface(metal_layer)
 			local surfaceCreateInfo = vk.VkMetalSurfaceCreateInfoEXT(
 				{
 					sType = "VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT",
@@ -71,21 +70,20 @@ do -- instance
 				vkCreateMetalSurfaceEXT(self.ptr[0], surfaceCreateInfo, nil, ptr),
 				"failed to create metal surface"
 			)
-			local surface = setmetatable({ptr = ptr, instance = self}, meta)
+			local surface = setmetatable({ptr = ptr, instance = self}, Surface)
 			return surface
 		end
 
-		function meta:__gc()
+		function Instance:__gc()
 			lib.vkDestroySurfaceKHR(self.instance.ptr[0], self.ptr[0], nil)
 		end
 	end
 
 	do -- physicalDevice
-		local meta_instance = meta
-		local meta = {}
-		meta.__index = meta
+		local PhysicalDevice = {}
+		PhysicalDevice.__index = PhysicalDevice
 
-		function meta_instance:GetPhysicalDevices()
+		function Instance:GetPhysicalDevices()
 			local deviceCount = ffi.new("uint32_t[1]", 0)
 			vk_assert(
 				lib.vkEnumeratePhysicalDevices(self.ptr[0], deviceCount, nil),
@@ -102,13 +100,13 @@ do -- instance
 			local out = {}
 
 			for i = 0, deviceCount[0] - 1 do
-				out[i + 1] = setmetatable({ptr = physicalDevices[i]}, meta)
+				out[i + 1] = setmetatable({ptr = physicalDevices[i]}, PhysicalDevice)
 			end
 
 			return out
 		end
 
-		function meta:FindGraphicsQueueFamily(surface)
+		function PhysicalDevice:FindGraphicsQueueFamily(surface)
 			local queueFamilyCount = ffi.new("uint32_t[1]", 0)
 			lib.vkGetPhysicalDeviceQueueFamilyProperties(self.ptr, queueFamilyCount, nil)
 			local queueFamilies = vk.Array(vk.VkQueueFamilyProperties)(queueFamilyCount[0])
@@ -138,15 +136,22 @@ do -- instance
 			return graphicsQueueFamily
 		end
 
-		function meta:GetSurfaceFormats(surface)
+		function PhysicalDevice:GetSurfaceFormats(surface)
 			local formatCount = ffi.new("uint32_t[1]", 0)
 			lib.vkGetPhysicalDeviceSurfaceFormatsKHR(self.ptr, surface.ptr[0], formatCount, nil)
-			local formats = vk.Array(vk.VkSurfaceFormatKHR)(formatCount[0])
+			local count = formatCount[0]
+			local formats = vk.Array(vk.VkSurfaceFormatKHR)(count)
 			lib.vkGetPhysicalDeviceSurfaceFormatsKHR(self.ptr, surface.ptr[0], formatCount, formats)
-			return formats
+
+			-- Convert to Lua table
+			local result = {}
+			for i = 0, count - 1 do
+				result[i + 1] = formats[i]
+			end
+			return result
 		end
 
-		function meta:GetSurfaceCapabilities(surface)
+		function PhysicalDevice:GetSurfaceCapabilities(surface)
 			local surfaceCapabilities = vk.Box(vk.VkSurfaceCapabilitiesKHR)()
 			vk_assert(
 				lib.vkGetPhysicalDeviceSurfaceCapabilitiesKHR(self.ptr, surface.ptr[0], surfaceCapabilities),
@@ -155,11 +160,26 @@ do -- instance
 			return surfaceCapabilities
 		end
 
-		do -- device
-			local meta_device = {}
-			meta_device.__index = meta_device
+		function PhysicalDevice:GetPresentModes(surface)
+			local presentModeCount = ffi.new("uint32_t[1]", 0)
+			lib.vkGetPhysicalDeviceSurfacePresentModesKHR(self.ptr, surface.ptr[0], presentModeCount, nil)
+			local count = presentModeCount[0]
+			local presentModes = vk.Array(vk.VkPresentModeKHR)(count)
+			lib.vkGetPhysicalDeviceSurfacePresentModesKHR(self.ptr, surface.ptr[0], presentModeCount, presentModes)
 
-			function meta:CreateDevice(extensions, graphicsQueueFamily)
+			-- Convert to Lua table
+			local result = {}
+			for i = 0, count - 1 do
+				result[i + 1] = presentModes[i]
+			end
+			return result
+		end
+
+		do -- device
+			local Device = {}
+			Device.__index = Device
+
+			function PhysicalDevice:CreateDevice(extensions, graphicsQueueFamily)
 				local queuePriority = ffi.new("float[1]", 1.0)
 				local queueCreateInfo = vk.Box(
 					vk.VkDeviceQueueCreateInfo,
@@ -186,25 +206,25 @@ do -- instance
 					lib.vkCreateDevice(self.ptr, deviceCreateInfo, nil, ptr),
 					"failed to create device"
 				)
-				local device = setmetatable({ptr = ptr}, meta_device)
+				local device = setmetatable({ptr = ptr}, Device)
 				return device
 			end
 
-			function meta_device:__gc()
+			function Device:__gc()
 				lib.vkDestroyDevice(self.ptr[0], nil)
 			end
 
 			do -- queue
-				local meta_queue = {}
-				meta_queue.__index = meta_queue
+				local Queue = {}
+				Queue.__index = Queue
 
-				function meta_device:GetQueue(graphicsQueueFamily)
+				function Device:GetQueue(graphicsQueueFamily)
 					local deviceQueue = vk.Box(vk.VkQueue)()
 					lib.vkGetDeviceQueue(self.ptr[0], graphicsQueueFamily, 0, deviceQueue)
-					return setmetatable({ptr = deviceQueue}, meta_queue)
+					return setmetatable({ptr = deviceQueue}, Queue)
 				end
 
-				function meta_queue:Submit(
+				function Queue:Submit(
 					commandBuffer,
 					imageAvailableSemaphore,
 					renderFinishedSemaphore,
@@ -235,29 +255,52 @@ do -- instance
 			end
 
 			do -- swapchain
-				local meta_swapchain = {}
-				meta_swapchain.__index = meta_swapchain
+				local Swapchain = {}
+				Swapchain.__index = Swapchain
 
-				function meta_device:CreateSwapchain(surface, surfaceFormat, surfaceCapabilities)
+				-- config options:
+				--   presentMode: VkPresentModeKHR (default: "VK_PRESENT_MODE_FIFO_KHR")
+				--   imageCount: number of images in swapchain (default: surfaceCapabilities.minImageCount)
+				--   compositeAlpha: VkCompositeAlphaFlagBitsKHR (default: "VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR")
+				--   clipped: boolean (default: true)
+				--   imageUsage: VkImageUsageFlags (default: COLOR_ATTACHMENT_BIT | TRANSFER_DST_BIT)
+				--   preTransform: VkSurfaceTransformFlagBitsKHR (default: currentTransform)
+				function Device:CreateSwapchain(surface, surfaceFormat, surfaceCapabilities, config)
+					config = config or {}
+					local imageCount = config.imageCount or surfaceCapabilities[0].minImageCount
+					local presentMode = config.presentMode or "VK_PRESENT_MODE_FIFO_KHR"
+					local compositeAlpha = config.compositeAlpha or "VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR"
+					local clipped = config.clipped ~= nil and (config.clipped and 1 or 0) or 1
+					local preTransform = config.preTransform or surfaceCapabilities[0].currentTransform
+					local imageUsage = config.imageUsage or bit.bor(
+						vk.VkImageUsageFlagBits("VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT"),
+						vk.VkImageUsageFlagBits("VK_IMAGE_USAGE_TRANSFER_DST_BIT")
+					)
+
+					-- Clamp image count to valid range
+					if imageCount < surfaceCapabilities[0].minImageCount then
+						imageCount = surfaceCapabilities[0].minImageCount
+					end
+					if surfaceCapabilities[0].maxImageCount > 0 and imageCount > surfaceCapabilities[0].maxImageCount then
+						imageCount = surfaceCapabilities[0].maxImageCount
+					end
+
 					local swapchainCreateInfo = vk.Box(
 						vk.VkSwapchainCreateInfoKHR,
 						{
 							sType = "VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR",
 							surface = surface.ptr[0],
-							minImageCount = surfaceCapabilities[0].minImageCount,
+							minImageCount = imageCount,
 							imageFormat = surfaceFormat.format,
 							imageColorSpace = surfaceFormat.colorSpace,
 							imageExtent = surfaceCapabilities[0].currentExtent,
 							imageArrayLayers = 1,
-							imageUsage = bit.bor(
-								vk.VkImageUsageFlagBits("VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT"),
-								vk.VkImageUsageFlagBits("VK_IMAGE_USAGE_TRANSFER_DST_BIT")
-							),
+							imageUsage = imageUsage,
 							imageSharingMode = "VK_SHARING_MODE_EXCLUSIVE",
-							preTransform = surfaceCapabilities[0].currentTransform,
-							compositeAlpha = vk.VkCompositeAlphaFlagBitsKHR("VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR"),
-							presentMode = "VK_PRESENT_MODE_FIFO_KHR",
-							clipped = 1,
+							preTransform = preTransform,
+							compositeAlpha = vk.VkCompositeAlphaFlagBitsKHR(compositeAlpha),
+							presentMode = presentMode,
+							clipped = clipped,
 							oldSwapchain = nil,
 						}
 					)
@@ -266,15 +309,15 @@ do -- instance
 						lib.vkCreateSwapchainKHR(self.ptr[0], swapchainCreateInfo, nil, ptr),
 						"failed to create swapchain"
 					)
-					local swapchain = setmetatable({ptr = ptr, device = self}, meta_swapchain)
+					local swapchain = setmetatable({ptr = ptr, device = self}, Swapchain)
 					return swapchain
 				end
 
-				function meta_swapchain:__gc()
+				function Swapchain:__gc()
 					lib.vkDestroySwapchainKHR(self.device.ptr[0], self.ptr[0], nil)
 				end
 
-				function meta_swapchain:GetImages()
+				function Swapchain:GetImages()
 					local imageCount = ffi.new("uint32_t[1]", 0)
 					lib.vkGetSwapchainImagesKHR(self.device.ptr[0], self.ptr[0], imageCount, nil)
 					local swapchainImages = vk.Array(vk.VkImage)(imageCount[0])
@@ -282,7 +325,7 @@ do -- instance
 					return swapchainImages
 				end
 
-				function meta_swapchain:GetNextImage(imageAvailableSemaphore)
+				function Swapchain:GetNextImage(imageAvailableSemaphore)
 					local imageIndex = ffi.new("uint32_t[1]", 0)
 					vk_assert(
 						lib.vkAcquireNextImageKHR(
@@ -298,7 +341,7 @@ do -- instance
 					return imageIndex
 				end
 
-				function meta_swapchain:Present(renderFinishedSemaphore, deviceQueue, imageIndex)
+				function Swapchain:Present(renderFinishedSemaphore, deviceQueue, imageIndex)
 					local presentInfo = vk.Box(
 						vk.VkPresentInfoKHR,
 						{
@@ -315,10 +358,10 @@ do -- instance
 			end
 
 			do -- commandPool
-				local meta_commandPool = {}
-				meta_commandPool.__index = meta_commandPool
+				local CommandPool = {}
+				CommandPool.__index = CommandPool
 
-				function meta_device:CreateCommandPool(graphicsQueueFamily)
+				function Device:CreateCommandPool(graphicsQueueFamily)
 					local commandPoolCreateInfo = vk.Box(
 						vk.VkCommandPoolCreateInfo,
 						{
@@ -332,19 +375,19 @@ do -- instance
 						lib.vkCreateCommandPool(self.ptr[0], commandPoolCreateInfo, nil, ptr),
 						"failed to create command pool"
 					)
-					local commandPool = setmetatable({ptr = ptr, device = self}, meta_commandPool)
+					local commandPool = setmetatable({ptr = ptr, device = self}, CommandPool)
 					return commandPool
 				end
 
-				function meta_commandPool:__gc()
+				function CommandPool:__gc()
 					lib.vkDestroyCommandPool(self.device.ptr[0], self.ptr[0], nil)
 				end
 
 				do -- commandBuffer
-					local meta_commandBuffer = {}
-					meta_commandBuffer.__index = meta_commandBuffer
+					local CommandBuffer = {}
+					CommandBuffer.__index = CommandBuffer
 
-					function meta_commandPool:CreateCommandBuffer()
+					function CommandPool:CreateCommandBuffer()
 						local commandBufferAllocInfo = vk.Box(
 							vk.VkCommandBufferAllocateInfo,
 							{
@@ -359,10 +402,10 @@ do -- instance
 							lib.vkAllocateCommandBuffers(self.device.ptr[0], commandBufferAllocInfo, commandBuffer),
 							"failed to allocate command buffer"
 						)
-						return setmetatable({ptr = commandBuffer}, meta_commandBuffer)
+						return setmetatable({ptr = commandBuffer}, CommandBuffer)
 					end
 
-					function meta_commandBuffer:Begin()
+					function CommandBuffer:Begin()
 						local beginInfo = vk.Box(
 							vk.VkCommandBufferBeginInfo,
 							{
@@ -376,11 +419,11 @@ do -- instance
 						)
 					end
 
-					function meta_commandBuffer:Reset()
+					function CommandBuffer:Reset()
 						lib.vkResetCommandBuffer(self.ptr[0], 0)
 					end
 
-					function meta_commandBuffer:CreateImageMemoryBarrier(imageIndex, swapchainImages)
+					function CommandBuffer:CreateImageMemoryBarrier(imageIndex, swapchainImages)
 						local barrier = vk.Box(
 							vk.VkImageMemoryBarrier,
 							{
@@ -404,7 +447,7 @@ do -- instance
 						return barrier
 					end
 
-					function meta_commandBuffer:StartPipelineBarrier(barrier)
+					function CommandBuffer:StartPipelineBarrier(barrier)
 						lib.vkCmdPipelineBarrier(
 							self.ptr[0],
 							vk.VkPipelineStageFlagBits("VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT"),
@@ -419,7 +462,7 @@ do -- instance
 						)
 					end
 
-					function meta_commandBuffer:EndPipelineBarrier(barrier)
+					function CommandBuffer:EndPipelineBarrier(barrier)
 						barrier[0].oldLayout = "VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL"
 						barrier[0].newLayout = "VK_IMAGE_LAYOUT_PRESENT_SRC_KHR"
 						barrier[0].srcAccessMask = vk.VkAccessFlagBits("VK_ACCESS_TRANSFER_WRITE_BIT")
@@ -438,17 +481,17 @@ do -- instance
 						)
 					end
 
-					function meta_commandBuffer:End()
+					function CommandBuffer:End()
 						vk_assert(lib.vkEndCommandBuffer(self.ptr[0]), "failed to end command buffer")
 					end
 				end
 			end
 
 			do -- semaphore
-				local meta_semaphore = {}
-				meta_semaphore.__index = meta_semaphore
+				local Semaphore = {}
+				Semaphore.__index = Semaphore
 
-				function meta_device:CreateSemaphore()
+				function Device:CreateSemaphore()
 					local semaphoreCreateInfo = vk.Box(
 						vk.VkSemaphoreCreateInfo,
 						{
@@ -460,20 +503,20 @@ do -- instance
 						lib.vkCreateSemaphore(self.ptr[0], semaphoreCreateInfo, nil, ptr),
 						"failed to create semaphore"
 					)
-					local semaphore = setmetatable({ptr = ptr, device = self}, meta_semaphore)
+					local semaphore = setmetatable({ptr = ptr, device = self}, Semaphore)
 					return semaphore
 				end
 
-				function meta_semaphore:__gc()
+				function Semaphore:__gc()
 					lib.vkDestroySemaphore(self.device.ptr[0], self.ptr[0], nil)
 				end
 			end
 
 			do -- fence
-				local meta_fence = {}
-				meta_fence.__index = meta_fence
+				local Fence = {}
+				Fence.__index = Fence
 
-				function meta_device:CreateFence()
+				function Device:CreateFence()
 					local fenceCreateInfo = vk.Box(
 						vk.VkFenceCreateInfo,
 						{
@@ -483,15 +526,15 @@ do -- instance
 					)
 					local ptr = vk.Box(vk.VkFence)()
 					vk_assert(lib.vkCreateFence(self.ptr[0], fenceCreateInfo, nil, ptr), "failed to create fence")
-					local fence = setmetatable({ptr = ptr, device = self}, meta_fence)
+					local fence = setmetatable({ptr = ptr, device = self}, Fence)
 					return fence
 				end
 
-				function meta_fence:__gc()
+				function Fence:__gc()
 					lib.vkDestroyFence(self.device.ptr[0], self.ptr[0], nil)
 				end
 
-				function meta_fence:Wait()
+				function Fence:Wait()
 					lib.vkWaitForFences(self.device.ptr[0], 1, self.ptr, 1, ffi.cast("uint64_t", -1))
 					lib.vkResetFences(self.device.ptr[0], 1, self.ptr)
 				end
