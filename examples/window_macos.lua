6b1cda7042ff16c8e6c85fcedab123b712b99f46
Elias Hogstvedt <eliashogstvedt@gmail.com>
2025-11-04 14:26:38 +0100

    window only example

---
+++ examples/window_macos.lua
@@ -0,0 +1,15 @@
+local cocoa = require("cocoa")
+local wnd = cocoa.window()wnd:Initialize()
+wnd:OpenWindow()
+
+while true do
+	local events = wnd:ReadEvents()
+
+	if events.window_close_requested then
+		print("Window close requested")
+
+		break
+	end
+
+	if events.window_resized then print("window resize") end
+end
